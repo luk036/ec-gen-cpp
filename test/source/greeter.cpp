88f26d737baa4f7bb3a468bb15aa87f03e1cb423
Wai-Shing Luk <luk036@gmail.com>
2023-08-06 08:36:45 +0000

    set indent 4

--- test/source/greeter.cpp
+++ test/source/greeter.cpp
@@ -5,17 +5,17 @@
 #include <string>
 
 TEST_CASE("EcGen") {
-  using namespace ecgen;
+    using namespace ecgen;
 
-  EcGen ecgen("Tests");
+    EcGen ecgen("Tests");
 
-  CHECK(ecgen.greet(LanguageCode::EN) == "Hello, Tests!");
-  CHECK(ecgen.greet(LanguageCode::DE) == "Hallo Tests!");
-  CHECK(ecgen.greet(LanguageCode::ES) == "¡Hola Tests!");
-  CHECK(ecgen.greet(LanguageCode::FR) == "Bonjour Tests!");
+    CHECK(ecgen.greet(LanguageCode::EN) == "Hello, Tests!");
+    CHECK(ecgen.greet(LanguageCode::DE) == "Hallo Tests!");
+    CHECK(ecgen.greet(LanguageCode::ES) == "¡Hola Tests!");
+    CHECK(ecgen.greet(LanguageCode::FR) == "Bonjour Tests!");
 }
 
 TEST_CASE("EcGen version") {
-  static_assert(std::string_view(ECGEN_VERSION) == std::string_view("1.0"));
-  CHECK(std::string(ECGEN_VERSION) == std::string("1.0"));
+    static_assert(std::string_view(ECGEN_VERSION) == std::string_view("1.0"));
+    CHECK(std::string(ECGEN_VERSION) == std::string("1.0"));
 }
