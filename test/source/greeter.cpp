6bcdd131b1eb933e5107ee2ed1bc6da5f8f8ea52
Wai-Shing Luk <luk036@gmail.com>
2023-09-11 16:54:40 +0800

    add -Wdeprecated-experimental-coroutine for Android's Termux

--- test/source/greeter.cpp
+++ test/source/greeter.cpp
@@ -1,6 +1,5 @@
 #include <doctest/doctest.h>
 #include <ecgen/greeter.h>
-#include <ecgen/version.h>
 
 #include <string>
 
@@ -15,7 +14,3 @@ TEST_CASE("EcGen") {
     CHECK(ecgen.greet(LanguageCode::FR) == "Bonjour Tests!");
 }
 
-TEST_CASE("EcGen version") {
-    static_assert(std::string_view(ECGEN_VERSION) == std::string_view("1.0"));
-    CHECK(std::string(ECGEN_VERSION) == std::string("1.0"));
-}
